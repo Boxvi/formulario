1848b4e722ae35e6f51eaec3a9c9247b203d46d3
Boxvi <hermosokawai@gmail.com>
2022-10-25 18:31:46 -0500

    con los formularios

--- src/main/java/formulario/controllers/FormController.java
+++ src/main/java/formulario/controllers/FormController.java
@@ -3,9 +3,13 @@ package formulario.controllers;
 import formulario.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 
@@ -17,6 +21,25 @@ public class FormController {
         return "form";
     }
 
+    @PostMapping("/form")
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+        model.addAttribute("titulo", "Resultado formulario");
+        if (result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
+
+
+        model.addAttribute("usuario", usuario);
+        return "resultado";
+    }
+
+
+
     /*
     @PostMapping("/form")
     public String procesar(Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email) {
@@ -25,8 +48,7 @@ public class FormController {
         model.addAttribute("password", password);
         model.addAttribute("email", email);
         return "resultado";
-    }*/
-
+    }
 
     @PostMapping("/form")
     public String procesar(Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email) {
@@ -35,6 +57,6 @@ public class FormController {
 
         model.addAttribute("usuario", usuario);
         return "resultado";
-    }
+    }*/
 
 }
--- src/main/java/formulario/domain/Usuario.java
+++ src/main/java/formulario/domain/Usuario.java
@@ -3,11 +3,17 @@ package formulario.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 public class Usuario {
+
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String email;
 
     public Usuario() {
