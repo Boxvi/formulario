fbe6f700a2d6f6946ad8e2a7137c13606329d14e
Boxvi <hermosokawai@gmail.com>
2022-10-25 20:29:55 -0500

    Anotaciones @Size e @Email

--- src/main/java/formulario/domain/Usuario.java
+++ src/main/java/formulario/domain/Usuario.java
@@ -3,7 +3,9 @@ package formulario.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -17,13 +19,15 @@ public class Usuario {
     @NotEmpty
     private String apellido;
 
-    @NotEmpty
+    @NotEmpty(message = "El usuario no puede estar vacio")
+    @Size(min = 3, max = 8)
     private String username;
 
     @NotEmpty
     private String password;
 
-    @NotEmpty
+    @NotEmpty(message = "El email no puede estar vacio, o no esta escrito correctamente")
+    @Email
     private String email;
 
     public Usuario() {
